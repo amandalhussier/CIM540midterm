528f026b0aff02355126df46c6b17bb43d961df0
Amanda L'Hussier <amandalhussier@Amandas-MacBook-Air-3.local>
2018-04-05 11:04:21 -0400

    everything

--- midtermpicture/sketch.js
+++ midtermpicture/sketch.js
@@ -1,73 +1,64 @@
-var button1;
-var button2;
-var button3;
-var button4;
-var hitImage;
+var mouseX;
+var mouseY;
 
-// hitzone variables
-var hitZoneX = 260;
-var hitZoneY = 800;
+var cheetah;
+var harpy;
+var scholar;
+var map;
 
-// keyPressed variable
-var value = 0;
+var button;
 
+var imageSize = 100;
+var bgImageSize = 1000;
+var blurbSize = 200;
 
 function preload(){
-  img = loadImage("koala.jpg");
-  hitImage = loadImage("ballons.png");
+  cheetah = loadImage("Chetah.png");
+  harpy = loadImage("Harpy.png");
+  scholar = loadImage("Scholarship.png");
+  map = loadImage("Map.png");
 }
 
 function setup() {
   createCanvas(1000,1000);
-// Creating Buttons - Can I make the function a link to a donation page?
-    button1 = createButton("Click here if you think we're cute");
-    button1.position(50,900);
-    button1.mousePressed(donatePage);
-    button2 = createButton("Click here if you don't");
-    button2.position(300,900);
-    button2.mousePressed(donatePage);
-    button3 = createButton("Click here if you want to donate");
-    button3.position(550,900);
-    button3.mousePressed(donatePage);
-    button4 = createButton("Click here if you are breathing");
-    button4.position(800,900);
-    button4.mousePressed(donatePage);
-// I want to turn this button function into a link
-
+//Need to make a donation button here
 }
 
-function donatePage() {
-var val = random(255);
-background(val);
-//location.href="http://www.google.com";
-}
+
 
 function draw() {
-      image(img,0,0);
-// Temporary mousePressed function until link is figured out
-      function mousePressed(){
-        backgroundColor = "blue";
-      }
-      function mouseReleased(){
-        backgroundColor = "red"
-      }
+  image(map, 0, 0, bgImageSize, bgImageSize);
+  image(cheetah, 530, 770, imageSize, imageSize); //cheetah
+  image(harpy, 130, 520, imageSize, imageSize); //harpy
+  image(scholar, 170, 380, imageSize, imageSize); //scholar
+
+//Cheetah Blurb
+if(mouseX >= 430 && mouseX < 630 && mouseY >= 670 && mouseY < 870) {
+    console.log("Cheetah");
+    fill(255);
+    rect(530, 770, blurbSize, blurbSize);
+    text("Sponsorship of Anatolian Shepherd dogs in South Africa through the nonprofit Cheetah Outreach. These dogs are bred to protect livestock from cheetah attacks with a success rate of over 85% and have reduced the number of cheetah casualties that arise from the human-wildlife conflict.");
+  }
+
+//Harpy Blurb
+if(mouseX >= 80 && mouseX < 180 && mouseY >= 420 && mouseY < 620) {
+    console.log("Harpy");
+    fill (255);
+    rect(130, 520, blurbSize, blurbSize);
+    text("Harpy eagle conservation project in Panama. This project spurred the 2002 law that designated the harpy eagle as the official bird of Panama.");
+  }
+
+//Scholarship Blurb
+if(mouseX >= 120 && mouseX < 220 && mouseY >= 330 && mouseY < 430) {
+    console.log("Scholarship");
+    fill (255);
+    rect(170, 380, blurbSize, blurbSize);
+    text("An annual $5,000 scholarship to a graduate student in the Department of Wildlife Ecology and Conservation at the University of Florida. This scholarship reflects Ron's deep commitment to training the next generation of conservationists.");
+  }
 
 
-// How do I get this hitzone to be whenever the cursor is over one of the buttons?
-      strokeWeight(1);
-      ellipse(hitZoneX, hitZoneY, 10, 10);
-      var hitZoneDist = dist(mouseX, mouseY, hitZoneX, hitZoneY);
 
-// console.log(hitZoneDist);
-      if (hitZoneDist < 10) {
-        image(hitImage, hitZoneX - 100, hitZoneY - 100);
-      }
 
-// Attempt to making a mouseX and mouseY console.log
-      if(mouseX > width/2){
-        console.log("Side 1");
-      }
-      if(mouseX < width/2){
-        console.log("Side 2");
-      }
+
+
 }
